afde6ab45c7c96ba779f3fc4394702badc4788a9
Adam Paterson <hello@adampaterson.co.uk>
2023-04-26 16:51:07 +0100

    Update composer scripts

--- rector.php
+++ rector.php
@@ -8,7 +8,7 @@ use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/tests',
+        __DIR__.'/tests',
     ]);
 
     // register a single rule
